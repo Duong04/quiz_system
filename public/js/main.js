5c91a66d7b16f91fe0031bf4c2c701330eac47bc
Duong <duongnt3092004@gmail.com>
2024-03-12 00:05:20 +0700

    Update source code

--- public/js/main.js
+++ public/js/main.js
@@ -4,4 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("mousemove", function(e) {
       dot.style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(1)`;
     });
-  });
+});
+
+
+const headerMain = document.querySelector('header');
+const linkChange = document.querySelector('.link-change');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) {
+    headerMain.classList.add('header-fixed');
+    linkChange.style.display = 'block';
+    headerMain.style.top = '0';
+} else if (window.scrollY < 200) {
+    headerMain.classList.remove('header-fixed');
+    linkChange.style.display = 'none';
+    headerMain.style.top = '-100px';
+}
+});
